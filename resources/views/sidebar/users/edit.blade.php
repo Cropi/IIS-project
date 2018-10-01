2e7ec3ba90718cc0a7d65cdb034c0d0a51f93feb
Cropi <xlakat01@stud.fit.vutbr.cz>
2018-10-01 22:44:56 +0200

    User.edit fix

--- resources/views/sidebar/users/edit.blade.php
+++ resources/views/sidebar/users/edit.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Update informations</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('update-user', [ 'id' => $user->id ]) }}">
                         {{ csrf_field() }}
